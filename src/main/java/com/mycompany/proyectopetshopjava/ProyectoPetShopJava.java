2ce96c07c96fa5ef90a2dfca35d4bf32d04f03a0
Jhonatan <Jhonatan@DESKTOP-CD207J1>
2024-10-26 00:33:27 -0500

    Carga inicial

---
+++ src/main/java/com/mycompany/proyectopetshopjava/ProyectoPetShopJava.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.proyectopetshopjava;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class ProyectoPetShopJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

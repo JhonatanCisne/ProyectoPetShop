8d4ad441c2e64ed162623a11ae0382b805495ec7
JhonatanCisne <jhonatanjorge151@gmail.com>
2024-10-27 12:19:27 -0500

    Update ProyectoPetShopJava.java
    PRUEBA

--- src/main/java/com/mycompany/proyectopetshopjava/ProyectoPetShopJava.java
+++ src/main/java/com/mycompany/proyectopetshopjava/ProyectoPetShopJava.java
@@ -9,6 +9,6 @@ package com.mycompany.proyectopetshopjava;
 public class ProyectoPetShopJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("HOAL MUNDO!");
     }
 }

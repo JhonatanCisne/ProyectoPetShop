a7e58fbed8cdb8cdd70783d836c5182444f84bea
Jhonatan <Jhonatan@DESKTOP-CD207J1>
2024-11-03 15:38:13 -0500

    Eliminando pruebas

--- src/main/java/com/mycompany/proyectopetshopjava/ProyectoPetShopJava.java
+++
@@ -1,9 +0,0 @@
-
-package com.mycompany.proyectopetshopjava;
-
-public class ProyectoPetShopJava {
-
-    public static void main(String[] args) {
-        System.out.println("HOLA MUNDO!");
-    }
-}
